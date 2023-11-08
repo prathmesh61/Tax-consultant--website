7704d25150eca82a9c98d73dab9cb80f4d6198ce
prathmesh <prathmeshpol61@gmail.com>
2023-11-08 14:43:08 +0530

    new HomeRegister component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import ExperthLayout from "@/components/base/ExperthLayout";
 import Footer from "@/components/base/Footer";
 import FormLayout from "@/components/base/FormLayout";
 import Header from "@/components/base/Header";
 import Hero from "@/components/base/Hero";
+import HomeRegister from "@/components/base/HomeRegister";
 import ServiceLayout from "@/components/base/ServiceLayout";
-import heroBg from "@/public/hero.jpg";
 import mainBG from "@/public/Herobg.png";
 import Image from "next/image";
 export default function Home() {
@@ -23,6 +22,7 @@ export default function Home() {
         </div>
       </div>
       <FormLayout />
+      <HomeRegister />
       <ServiceLayout />
       <Footer />
     </main>
---
+++ components/base/HomeRegister.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import secondHeroBg from "@/public/HeroImage.jpg";
+import Image from "next/image";
+import { ArrowBigDownDash, ArrowBigRightDash, Check } from "lucide-react";
+
+const HomeRegister = () => {
+  const list: string[] = [
+    "Complete Solution",
+    "Time saving process",
+    "Expert team",
+    "Deed preparation",
+    " valuation of property & stamp duty",
+    "Discounted legal charges",
+  ];
+  return (
+    <div className="h-screen w-screen relative mt-20">
+      <Image
+        alt="secondHeroBg"
+        src={secondHeroBg}
+        className="object-cover object-center w-full h-full "
+      />
+      <div className="absolute top-0 bottom-0 left-0 w-[65%] bg-black opacity-60 blur-2xl" />
+      <div className="flex flex-col gap-y-8 absolute left-5 lg:left-60 top-80 max-w-[1440px]  mx-auto ">
+        <div className="w-full">
+          <h2 className="text-white whitespace-nowrap  font-extrabold lg:text-5xl text-[16px]  before:border-l-8  before:mr-3 before:border-brand">
+            Property Registration: Importance,
+          </h2>
+          <h2 className="text-white whitespace-nowrap   lg:text-5xl text-[16px]  font-extrabold before:border-l-8  before:mr-3 before:border-brand">
+            Process, and Legal Requirements
+          </h2>
+        </div>
+        <div>
+          <p className="text-white font-medium text-sm lg:text-lg max-w-[458px] mb-4 flex items-center  gap-2">
+            Why choose us? <ArrowBigDownDash className="w-5 h-5 text-brand" />
+          </p>
+          <ul className="grid grid-cols-2  lg:max-w-[60%] w-full gap-2">
+            {list.map((item: string) => (
+              <li
+                key={item}
+                className="text-[10px] lg:text-sm font-bold flex items-start gap-1 text-white"
+              >
+                <Check className="text-brand" size={20} />
+                {item}
+              </li>
+            ))}
+          </ul>
+          <a
+            href="#formLayout"
+            className="w-[150px] h-[30px] lg:w-[250px] lg:h-[40px] bg-brand text-[10px] lg:text-sm cursor-pointer rounded-md flex items-center justify-center gap-2 mt-4"
+          >
+            Make Appointment <ArrowBigRightDash className="w-5 h-5 " />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeRegister;
